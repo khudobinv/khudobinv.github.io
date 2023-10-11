f359a8bbc8911f9a075b90f35452f834b0d5a7c2
Vasiliy Khudobin <146813280+khudobinv@users.noreply.github.com>
2023-10-11 11:53:38 +0300

    Fix count input

--- tasks/task5/script.js
+++ tasks/task5/script.js
@@ -1,20 +1,23 @@
 function func() {
     var sel = document.getElementById('product').selectedIndex;
     var result;
-    var count = Number(document.getElementById('count').value);
-    switch(sel) {
-        case 0:
-            result = 15292 * count;
-            break;
-        case 1:
-            result = 24990 * count;
-            break;
-        case 2:
-            result = 11999 * count;
-            break;
-        case 3:
-            result = 18990 * count;
-            break;
+    var count = document.getElementById('count').value;
+    if (/^[0-9]+$/.test(count)) {
+        count = Number(count);
+        switch(sel) {
+            case 0:
+                result = 15292 * count;
+                break;
+            case 1:
+                result = 24990 * count;
+                break;
+            case 2:
+                result = 11999 * count;
+                break;
+            case 3:
+                result = 18990 * count;
+                break;
+        }
     }
     document.getElementById("result").innerHTML = result;
 }
