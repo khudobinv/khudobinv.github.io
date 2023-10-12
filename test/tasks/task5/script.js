dc3c9b6b79c0f17da29ac0ef8d11a9ed0c2f78e3
khudobinv <146813280+khudobinv@users.noreply.github.com>
2023-10-12 12:00:32 +0300

    add task5

---
+++ test/tasks/task5/script.js
@@ -0,0 +1,26 @@
+function func() {
+    var sel = document.getElementById('product').selectedIndex;
+    var result;
+    var count = document.getElementById('count').value;
+    if (!(/^[0-9]+$/.test(count))) {
+        result = "ни сколько";
+    }
+    else {
+        count = Number(count);
+        switch(sel) {
+            case 0:
+                result = 15292 * count;
+                break;
+            case 1:
+                result = 24990 * count;
+                break;
+            case 2:
+                result = 11999 * count;
+                break;
+            case 3:
+                result = 18990 * count;
+                break;
+        }
+    }
+    document.getElementById("result").innerHTML = result;
+}
